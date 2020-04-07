ce585bb7373253a5417221a5def7f9a740fccf00
Leeroy Phili <leeroyphili@gmail.com>
2020-04-07 14:50:36 -0400

    added portfolio pics

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-$('#button').click(function(){
+$('#portfolio').click(function(){
+    $('#about').hide 
 
     console.log("okay lets get started")
-})
+});
+
+$('#contact').click(function(){
+
+    console.log("okay lets get started")
+});
+
+$('#about').click(function(){
+
+    console.log("okay lets get started")
+});
